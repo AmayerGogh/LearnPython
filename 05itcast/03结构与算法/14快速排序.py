c2f5cbb25df8244780f6d82b6b45916979c3a856
amayer <454593034@qq.com>
2018-02-10 18:08:19 +0800

    end

---
+++ 05itcast/03结构与算法/14快速排序.py
@@ -0,0 +1,7 @@
+def shell_sort(alist):
+    count =len(alist)
+    for j in range(0,count):
+        
+
+new_list =selection_sort([21,345,673,3,123,234,67,5,3,1,534,5676,223,23,57,9,67,34212,34,7,43])
+print(new_list)    
